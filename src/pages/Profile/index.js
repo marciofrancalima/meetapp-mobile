ca43b4b62eebd51db2069d8ac19920c557a5b577
Márcio França Lima <marciofrancalima@hotmail.com>
2019-08-17 16:43:11 -0300

    Updated Profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,129 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Text } from 'react-native';
 
-import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  Separator,
+  SubmitButton,
+  LogoutButton,
+} from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.user.loading);
+
+  const profile = useSelector(state => state.user.profile);
+
+  const [name, setName] = useState(profile.name);
+  const [email, setEmail] = useState(profile.email);
+  const [oldPassword, setOldPassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const emailRef = useRef();
+  const oldPasswordRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  useEffect(() => {
+    setOldPassword('');
+    setPassword('');
+    setConfirmPassword('');
+  }, [profile]);
+
+  function handleSubmit() {
+    dispatch(
+      updateProfileRequest({
+        name,
+        email,
+        oldPassword,
+        password,
+        confirmPassword,
+      })
+    );
+  }
+
+  function handleLogout() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
-      <Text>Meu perfil</Text>
+      <Form>
+        <FormInput
+          icon="people-outline"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Nome completo"
+          returnKeyType="next"
+          onSubmitEditing={() => emailRef.current.focus()}
+          value={name}
+          onChangeText={setName}
+        />
+
+        <FormInput
+          icon="mail-outline"
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Digite seu e-mail"
+          ref={emailRef}
+          returnKeyType="next"
+          onSubmitEditing={() => oldPasswordRef.current.focus()}
+          value={email}
+          onChangeText={setEmail}
+        />
+
+        <Separator />
+
+        <FormInput
+          icon="lock-outline"
+          secureTextEntry
+          placeholder="Sua senha secreta"
+          ref={oldPasswordRef}
+          returnKeyType="next"
+          onSubmitEditing={() => passwordRef.current.focus()}
+          value={oldPassword}
+          onChangeText={setOldPassword}
+        />
+
+        <FormInput
+          icon="lock-outline"
+          secureTextEntry
+          placeholder="Sua senha secreta"
+          ref={passwordRef}
+          returnKeyType="next"
+          onSubmitEditing={() => confirmPasswordRef.current.focus()}
+          value={password}
+          onChangeText={setPassword}
+        />
+
+        <FormInput
+          icon="lock-outline"
+          secureTextEntry
+          placeholder="Sua senha secreta"
+          ref={confirmPasswordRef}
+          returnKeyType="send"
+          onSubmitEditing={handleSubmit}
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        />
+
+        <SubmitButton loading={loading} onPress={handleSubmit}>
+          Atualizar perfil
+        </SubmitButton>
+
+        <LogoutButton loading={loading} onPress={handleLogout}>
+          Sair do Meetapp
+        </LogoutButton>
+      </Form>
     </Container>
   );
 }
@@ -17,4 +133,10 @@ Profile.navigationOptions = {
   tabBarIcon: ({ tintColor }) => (
     <Icon name="person" size={20} color={tintColor} />
   ),
+};
+
+Profile.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
 };
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,7 +1,42 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
+export const Container = styled.KeyboardAvoidingView.attrs({
+  enabled: Platform.OS === 'ios',
+  behavior: 'padding',
+})`
   background: #402845;
 
   flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Form = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { padding: 30 },
+})`
+  align-self: stretch;
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 10px;
+`;
+
+export const Separator = styled.View`
+  height: 1px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 20px 0 30px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 5px;
+`;
+
+export const LogoutButton = styled(Button)`
+  margin-top: 10px;
+  background: #f64c75;
 `;
